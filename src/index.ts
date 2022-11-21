d16c44b765e2701f49595ae3f2dc8fb2881c4b25
FinalAshen <819712530@qq.com>
2022-11-21 17:14:55 +0800

    fix: branch segmentation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import type { PluginOption, UserConfig } from 'vite';
-import MagicString from 'magic-string';
-import type { ImportSpecifier } from 'es-module-lexer'
-import { init, parse as parseImports } from 'es-module-lexer';
 
 interface ExternalExtensionType {
   [key: string]: {
@@ -21,56 +18,7 @@ const isExternalUrl = (url: string) => externalRE.test(url);
 
 let tempConfig = {} as UserConfig;
 
-const externalSubModuleRegExpList: [RegExp, string, string][] = [];
-
 const exclude: Array<string | RegExp> = [];
-
-function externalsExtensionTransform() {
-  return {
-    name: 'vite-plugin-externals-extension-transform',
-    enforce: EnForceType.POST,
-
-    async transform (source: string) {
-      await init
-      let imports: readonly ImportSpecifier[] = []
-      try {
-        imports = parseImports(source)[0]
-      } catch (e) {
-        
-      }
-      if (!imports.length) {
-        return null
-      }
-      let s: MagicString | undefined;
-      const str = () => s || (s = new MagicString(source))
-      for (let index = 0; index < imports.length; index++) {
-        const {
-          s: start,
-          e: end,
-          n: specifier
-        } = imports[index];
-        let externalSubModuleInfo: [RegExp, string, string] | undefined;
-        if (specifier && (externalSubModuleInfo = externalSubModuleRegExpList.find(regExp => regExp[0].test(specifier)))) {
-          const newSpecifier = externalSubModuleInfo[2] + specifier.substring(externalSubModuleInfo[1].length);
-          str().overwrite(
-            start,
-            end,
-            newSpecifier,
-            {
-              contentOnly: true
-            }
-          )
-        }
-      }
-      if (s) {
-        return {
-          code: s.toString(),
-          map: tempConfig.build?.sourcemap ? s.generateMap({ hires: true }) : null
-        }
-      }
-    }
-  }
-}
 
 function externalsExtensionResolverConfigFilter(): PluginOption {
   return {
@@ -107,10 +55,7 @@ function externalsExtensionResolver(options: ExternalExtensionType): PluginOptio
       exclude.push(
         ...await Promise.all(
           Object.entries(options).filter((option) => !option[1].getter).map(async (option) => {
-            // const externalSubModuleRegExp = new RegExp(`${option[0]}\/`);
-            // exclude.push(externalSubModuleRegExp);
             option[1].url = typeof option[1].url === "function" ? await option[1].url() : option[1].url;
-            // externalSubModuleRegExpList.push([externalSubModuleRegExp, option[0], option[1].url]);
             return option[1].url;
           })
         )
